4c8c9d33d019b3ebd8174c4ee5866e27fc37de96
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-09-20 12:37:11 +0300

    Add files via upload

---
+++ 2. Exam/src/utils/utils.js
@@ -0,0 +1,25 @@
+
+export function getUserData(){
+    return JSON.parse(sessionStorage.getItem('userData'))
+}
+
+export function setUserData(data){
+    return sessionStorage.setItem('userData', JSON.stringify(data))
+}
+
+export function clearUserData(){
+    return sessionStorage.removeItem('userData')
+}
+
+export function updateNav(){
+    const userData = getUserData()
+
+    document.querySelector('.user').style.display = userData ? '' : 'none'
+    document.querySelector('.guest').style.display = userData ? 'none' : ''
+
+    // if (userData)
+    //     document.querySelector('user'){
+    // }
+
+
+}
