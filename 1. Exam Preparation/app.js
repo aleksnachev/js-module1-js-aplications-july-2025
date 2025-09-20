55f346ef0fd3331f19a444eecac58979e6db3412
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-09-20 12:30:14 +0300

    Add files via upload

---
+++ 1. Exam Preparation/app.js
@@ -0,0 +1,33 @@
+import { logout } from './data/user.js'
+import {page} from './lib.js'
+import {addRender} from './utils/render.js'
+import { updateNav } from './utils/utils.js'
+import { catalogView } from './views/catalog.js'
+import { createView } from './views/create.js'
+import { detailsView } from './views/details.js'
+import { editView } from './views/edit.js'
+
+import {homeView} from './views/home.js'
+import { loginView } from './views/login.js'
+import { registerView } from './views/register.js'
+
+updateNav()
+document.getElementById('logoutBtn').addEventListener('click', ()=>{
+    logout()
+    updateNav()
+    page.redirect('/')
+})
+
+
+page(addRender)
+
+//TODO Bind projects URLs to view handlers
+page('/', homeView)
+page('/catalog', catalogView)
+page('/catalog/:id', detailsView)
+page('/edit/:id', editView)
+page('/create', createView)
+page('/login', loginView)
+page('/register', registerView)
+
+page.start()
